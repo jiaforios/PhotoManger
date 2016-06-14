238ed529871f147481173ab7440e51efe7995df4
jiaforios <jiaforios@163.com>
2016-06-14 14:23:30 +0800

    Photomanger
    Application for photoManger

---
+++ PhotoManger/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PhotoManger
+//
+//  Created by foscom on 16/6/14.
+//  Copyright © 2016年 zengjia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ PhotoMangerTests/PhotoMangerTests.m
@@ -0,0 +1,39 @@
+//
+//  PhotoMangerTests.m
+//  PhotoMangerTests
+//
+//  Created by foscom on 16/6/14.
+//  Copyright © 2016年 zengjia. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PhotoMangerTests : XCTestCase
+
+@end
+
+@implementation PhotoMangerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
